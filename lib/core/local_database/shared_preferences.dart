4fcc919e780865356a5f87e87982549698fc7052
IzraelAA <acari.panca21@gmail.com>
2023-11-20 00:22:00 +0700

    chore(star-wars-app): add delete caching after 1 week

--- lib/core/local_database/shared_preferences.dart
+++ lib/core/local_database/shared_preferences.dart
@@ -11,12 +11,21 @@ class StarWarsLocalDatabase {
   Future<bool> setPeopleData(List<PeopleModel> data) async {
     final prefs = await _getSharedPreferences();
     final encodedData = data.map((person) => person.toJson()).toList();
+    // set date time
+    prefs.setInt('storedTime', DateTime.now().millisecondsSinceEpoch);
     return prefs.setStringList(
         'peopleData', encodedData.map((e) => json.encode(e)).toList());
   }
 
   Future<List<PeopleModel>> getPeopleData() async {
     final prefs = await _getSharedPreferences();
+    int storedTimeMillis = prefs.getInt('storedTime') ?? 0;
+    DateTime oneWeekAgo = DateTime.now().subtract(const Duration(days: 7));
+    DateTime storedTime = DateTime.fromMillisecondsSinceEpoch(storedTimeMillis);
+    // remove all data cache after 1 week
+    if (storedTime.isBefore(oneWeekAgo)) {
+      prefs.clear();
+    }
     final encodedData = prefs.getStringList('peopleData');
     if (encodedData != null) {
       return encodedData
